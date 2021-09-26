a439954d7fe93f2d2a218ed6e6e509fb7476458e
Zahari Mikov <zaharimikov2001@gmail.com>
2021-09-26 08:27:01 +0300

    Adding the Vote and Comment join tables

---
+++ src/main/java/com/freshvotes/freshvotes/domain/VoteId.java
@@ -0,0 +1,30 @@
+package com.freshvotes.freshvotes.domain;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+public class VoteId implements Serializable {
+    public static final long serialVersionUID = 6283918475234581923L;
+    private User user;
+    private Feature feature;
+
+    @ManyToOne
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @ManyToOne
+    public Feature getFeature() {
+        return feature;
+    }
+
+    public void setFeature(Feature feature) {
+        this.feature = feature;
+    }
+}
