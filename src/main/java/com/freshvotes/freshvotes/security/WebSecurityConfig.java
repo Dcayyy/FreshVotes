bd08d71c5ac1714b141d16d81876d339520fbca1
Zahari Mikov <zaharimikov2001@gmail.com>
2021-09-27 17:22:18 +0300

    Adding password encoder

--- src/main/java/com/freshvotes/freshvotes/security/WebSecurityConfig.java
+++ src/main/java/com/freshvotes/freshvotes/security/WebSecurityConfig.java
@@ -1,18 +1,27 @@
 package com.freshvotes.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
+                .passwordEncoder(getPasswordEncoder())
                 .withUser("zaharimikov2001@gmail.com")
-                .password("asdfasdf")
+                .password(getPasswordEncoder().encode("asdfasdf"))
                 .roles("USER");
     }
 
@@ -21,12 +30,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/").permitAll()
-                .antMatchers("/login").permitAll()
                     .anyRequest().hasRole("USER").and()
-/*                .formLogin()
+                .formLogin()
                     .loginPage("/login")
                     .permitAll()
-                    .and()*/
+                    .and()
                 .logout()
                     .logoutUrl("/logout")
                     .permitAll();
